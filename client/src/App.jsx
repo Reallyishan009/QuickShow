51f1bca9f3da62876ecf3729ae2c7627c092ed52
Reallyishan009 <ishantripathidoc@gmail.com>
2025-08-10 16:46:35 +0530

    deploy 21

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
@@ -17,6 +15,7 @@ import ListBookings from './pages/admin/ListBookings'
 import { useAppContext } from './context/AppContext'
 import { SignIn } from '@clerk/clerk-react'
 import Loading from './components/Loading'
+import Navbar from './components/Navbar'
 
 const App = () => {
 
